76745c6284f246a1549f2785546e50afaa70cfb1
KastSet <stupakkostya23@hmail.com>
2023-03-20 16:04:00 +0200

    Initial commit

---
+++ app/src/main/java/ua/kastset/todolistformybest/DialogCloseListner.java
@@ -0,0 +1,7 @@
+package ua.kastset.todolistformybest;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListner {
+    public void handleDialogClose(DialogInterface dialog);
+}
